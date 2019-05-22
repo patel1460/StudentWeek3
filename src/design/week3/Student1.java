015e63281321c030943e3e17fda0a816e8959ec5
Siddharth Patel <pate14602sheridancollege.ca>
2019-05-22 09:40:31 -0400

    Initial Commit

---
+++ src/design/week3/Student1.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package design.week3;
+
+/**
+ *
+ * @author Savan
+ */
+public class Student1 {
+ private int ID;
+ private String firstName;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    
+}
