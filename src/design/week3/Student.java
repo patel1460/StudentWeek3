19f898a864ca502f884b4e4b38b9f90957123730
Siddharth Patel <pate14602sheridancollege.ca>
2019-05-22 09:38:46 -0400

    Initial Commit

---
+++ src/design/week3/Student.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package design.week3;
+
+/**
+ *
+ * @author Savan
+ */
+public class Student {
+    public static void main(String[] args) {
+        Student1 s1=new Student1();
+        s1.setFirstName("John");
+        s1.setID(4);
+    }
+    
+}
